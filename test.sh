474fae7de2d89d5266338deb73956d66d0bfa37d
Your Name <you@example.com>
2026-05-18 20:38:02 -0700

    gt

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-python3 reference_submission/run.py generate     --requests reference_submission/problem/generate_free.jsonl
+# python3 reference_submission/run.py generate     --requests reference_submission/problem/generate_free.jsonl
 # python3 reference_submission/run.py generate     --requests /inspire/hdd/project/26summer-camp-03/26210833/Summer-Camp-Projects/reference_submission/problem/generate_requests.jsonl
-python3 reference_submission/run.py generate     --requests /inspire/hdd/project/26summer-camp-03/26210833/Summer-Camp-Projects/reference_submission/problem/generate_sample.jsonl
+# python3 reference_submission/run.py generate     --requests /inspire/hdd/project/26summer-camp-03/26210833/Summer-Camp-Projects/reference_submission/problem/generate_sample.jsonl
 # python3 reference_submission/run.py review     --requests /inspire/hdd/project/26summer-camp-03/26210833/Summer-Camp-Projects/reference_submission/problem/review_sample_gt.jsonl
 # python3 reference_submission/run.py review     --requests /inspire/hdd/project/26summer-camp-03/26210833/Summer-Camp-Projects/reference_submission/problem/review_sample.jsonl
 # python3 reference_submission/run.py review     --requests /inspire/hdd/project/26summer-camp-03/26210833/Summer-Camp-Projects/reference_submission/problem/review_train_gt.jsonl
